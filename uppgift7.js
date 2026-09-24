cc543e8a26302563258c6ff883e44f07aac8a9c3
Jannike Ljusner <din.mail@exempel.se>
2026-09-24 14:41:28 +0200

    Skapade funktion och loop för att summera array i uppgift 7

--- uppgift7.js
+++ uppgift7.js
@@ -1,2 +1,21 @@
 "use strict";
 /* Lösning till uppgift 7 av Jannike Ljusner */
+
+// 1. Array med tal
+let numbers = [10, 5, 145, 56, 69, 89];
+
+// 2. Funktion som tar emot en array (arr)
+function sumArray(arr) {
+  let sum = 0; // Nollställer miniräknaren
+
+  // Loop som plussar ihop alla tal
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  // Skickar tillbaka det färdiga svaret
+  return sum;
+}
+
+// 3. Utskriften: anropar funktionen med array 'numbers'
+console.log(sumArray(numbers));
