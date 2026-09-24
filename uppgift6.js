a402097de5904f612a19601df80bb9ca1432f084
Jannike Ljusner <din.mail@exempel.se>
2026-09-24 14:33:15 +0200

    Skapade en funktion för areaberäkning i uppgift 6

--- uppgift6.js
+++ uppgift6.js
@@ -1,2 +1,10 @@
 "use strict";
 /* Lösning till uppgift 6 av Jannike Ljusner */
+
+// Funktion som räknar ut arean utifrån bredd och längd
+function calculateArea(width, length) {
+  return width * length;
+}
+
+// Anropar funktionen med 5 och 20, och skriver ut resultatet i terminalen
+console.log(calculateArea(5, 20));
