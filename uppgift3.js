230e715aa58be3ad4fcbddf7eb90bfa978e0e6fd
Jannike Ljusner <din.mail@exempel.se>
2026-09-24 12:52:38 +0200

    Lade till villkor för ålderskontroll i uppgift 3

--- uppgift3.js
+++ uppgift3.js
@@ -1,2 +1,11 @@
 "use strict";
 /* Lösning till uppgift 3 av Jannike Ljusner */
+let age = 75;
+
+if (age < 18) {
+  console.log("Barn");
+} else if (age < 65) {
+  console.log("Vuxen");
+} else {
+  console.log("Pensionär");
+}
