04afc6ebd269fba92c77c51221aaa6d44a684ad4
Jannike Ljusner <din.mail@exempel.se>
2026-09-24 13:18:10 +0200

    Skapade en array med maträtter och använde array-metoder i uppgift 5

--- uppgift3.js
+++ uppgift3.js
@@ -6,12 +6,13 @@ let age = 15;
 
 // Kollar om åldern är under 18 år, då anges personen som barn
 if (age < 18) {
-    console.log("Barn");
-} 
+  console.log("Barn");
+}
 // Om personen inte är under 18, kollar vi om åldern är under 65, då anges personen som vuxen
 else if (age < 65) {
-    console.log("Vuxen");
-} 
+  console.log("Vuxen");
+}
 // Om inget av ovanstående stämmer är personen 65 eller äldre och anges som pensionär
 else {
-    console.log("Pensionär");
+  console.log("Pensionär");
+}
--- uppgift5.js
+++ uppgift5.js
@@ -1,2 +1,22 @@
 "use strict";
 /* Lösning till uppgift 5 av Jannike Ljusner */
+// Skapar en array med fem valfria maträtter
+let food = ["Pölsa", "Tacos", "Blodpudding", "Soppa", "Pizza"];
+
+// Skriver ut hela arrayen
+console.log(food);
+
+// Skriver ut det första elementet Pölsa (index 0)
+console.log(food[0]);
+
+// Skriver ut det sista elementet Pizza (index 4)
+console.log(food[4]);
+
+// Lägger till en ny maträtt sist i arrayen
+food.push("Lasagne");
+
+// Tar bort den första maträtten i arrayen (Pölsa)
+food.shift();
+
+// Skriver ut arrayen igen för att se förändringarna
+console.log(food);
