288337046a7244d77c618801a68b2b0c13cc155b
Jannike Ljusner <din.mail@exempel.se>
2026-09-24 14:50:50 +0200

    Lade till objekt och utskrift av bokinformation i uppgift 8

--- uppgift8.js
+++ uppgift8.js
@@ -1,2 +1,19 @@
 "use strict";
 /* Lösning till uppgift 8 av Jannike Ljusner */
+
+// 1. Vald Bok
+let myBook = {
+  titel: "It",
+  författare: "Stephen King",
+  utgivningsår: 1986,
+};
+
+// 2. Funktionen som skriver ut informationen
+function printBook(book) {
+  console.log("Titel: " + book.titel);
+  console.log("Författare: " + book.författare);
+  console.log("Utgivningsår: " + book.utgivningsår);
+}
+
+// 3. Skickar
+printBook(myBook);
