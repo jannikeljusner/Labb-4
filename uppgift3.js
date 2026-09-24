e624a0044d253691069e9436f9321476fb927390
Jannike Ljusner <din.mail@exempel.se>
2026-09-24 13:06:46 +0200

    Lade till en loop och ett villkor för att skriva ut jämna tal i uppgift 4

--- uppgift3.js
+++ uppgift3.js
@@ -1,11 +1,17 @@
 "use strict";
 /* Lösning till uppgift 3 av Jannike Ljusner */
-let age = 75;
 
+// Skapar en variabel för att spara en valfri ålder
+let age = 15;
+
+// Kollar om åldern är under 18 år, då anges personen som barn
 if (age < 18) {
-  console.log("Barn");
-} else if (age < 65) {
-  console.log("Vuxen");
-} else {
-  console.log("Pensionär");
-}
+    console.log("Barn");
+} 
+// Om personen inte är under 18, kollar vi om åldern är under 65, då anges personen som vuxen
+else if (age < 65) {
+    console.log("Vuxen");
+} 
+// Om inget av ovanstående stämmer är personen 65 eller äldre och anges som pensionär
+else {
+    console.log("Pensionär");
--- uppgift4.js
+++ uppgift4.js
@@ -1,2 +1,11 @@
 "use strict";
 /* Lösning till uppgift 4 av Jannike Ljusner */
+
+// Startar en loop som räknar från 1 upp till och med 20
+for (let i = 1; i <= 20; i++) {
+  // Använder modulus för att kontrollera om talet går att dela jämnt på 2
+  if (i % 2 === 0) {
+    // Skriver ut siffran om påståendet ovan är sant
+    console.log(i);
+  }
+}
