8206fdc3f29494c1edb8979b096717c962a518ae
Jannike Ljusner <din.mail@exempel.se>
2026-09-24 11:32:41 +0200

    Skapade variabler och utskrift i uppgift 1

---
+++ uppgift1.js
@@ -0,0 +1,11 @@
+"use strict";
+/* Lösning till uppgift 1 av Jannike Ljusner */
+// Skapar variabler för personuppgifter
+const firstName = "Jannike";
+const lastName = "Ljusner";
+let age = 37;
+const isStudent = true;
+// Skriver ut resultatet i terminalen
+console.log(firstName, lastName);
+console.log("Ålder:", age);
+console.log("Student:", isStudent);
